252c30ed96b4820f3ad01eee8c67c1c74cb2b152
unknown <sebastesb1986@gmail.com>
2022-04-21 18:15:11 -0500

    update Prueba ADA matriculas Salgado

--- resources/views/layouts/principal.blade.php
+++ resources/views/layouts/principal.blade.php
@@ -88,7 +88,7 @@
               <!-- Nav Item - User Information -->
               <li class="nav-item dropdown no-arrow">
                 <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  <span class="mr-2 d-none d-lg-inline text-gray-600 small">SISTEMA DE MATRICULA DE ALUMNOS</span>
+                  <span class="mr-2 d-none d-lg-inline text-gray-600 small"><b>SISTEMA DE MATRICULA DE ALUMNOS</b></span>
                  
                 </a>
                 <!-- Dropdown - User Information -->
